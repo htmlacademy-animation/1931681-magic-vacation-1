16706d2b4e5147ac2b2ec98f0b7ebeaa3f7ae945
Egor Prilipko <nero.36@mail.ru>
2021-11-25 20:30:40 +0300

    module1-task3: added page change transition (from story to prizes)

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -3,13 +3,17 @@ import throttle from 'lodash/throttle';
 export default class FullPageScroll {
   constructor() {
     this.THROTTLE_TIMEOUT = 1000;
+    this.PAGE_CHANGE_TRANSITION_TIME = 700;
+    this.PAGE_ACTIVE_DELAY = 100;
     this.scrollFlag = true;
     this.timeout = null;
 
     this.screenElements = document.querySelectorAll(`.screen:not(.screen--result)`);
     this.menuElements = document.querySelectorAll(`.page-header__menu .js-menu-link`);
+    this.transitions = [ { from: 1, to: 2 } ] // story -> prizes transition
 
     this.activeScreen = 0;
+    this.previousScreen = 0;
     this.onScrollHandler = this.onScroll.bind(this);
     this.onUrlHashChengedHandler = this.onUrlHashChanged.bind(this);
   }
@@ -41,6 +45,7 @@ export default class FullPageScroll {
 
   onUrlHashChanged() {
     const newIndex = Array.from(this.screenElements).findIndex((screen) => location.hash.slice(1) === screen.id);
+    this.previousScreen = this.activeScreen;
     this.activeScreen = (newIndex < 0) ? 0 : newIndex;
     this.changePageDisplay();
   }
@@ -52,14 +57,33 @@ export default class FullPageScroll {
   }
 
   changeVisibilityDisplay() {
-    this.screenElements.forEach((screen) => {
-      screen.classList.add(`screen--hidden`);
+    const addChangeTransition = this.addPageChangeTransition();
+
+    this.screenElements.forEach((screen, i) => {
+      if (addChangeTransition && i === this.previousScreen) {
+        screen.classList.add('exiting');
+      } else {
+        screen.classList.add(`screen--hidden`);
+      }
+      
       screen.classList.remove(`active`);
     });
-    this.screenElements[this.activeScreen].classList.remove(`screen--hidden`);
-    setTimeout(() => {
-      this.screenElements[this.activeScreen].classList.add(`active`);
-    }, 100);
+    
+    if (addChangeTransition) {
+
+      setTimeout(() => {
+        this.screenElements[this.activeScreen].classList.remove(`screen--hidden`);
+        this.screenElements[this.activeScreen].classList.add(`active`);
+
+        this.screenElements[this.previousScreen].classList.remove('exiting');
+        this.screenElements[this.previousScreen].classList.add(`screen--hidden`);
+      }, this.PAGE_CHANGE_TRANSITION_TIME);
+    } else {
+      this.screenElements[this.activeScreen].classList.remove(`screen--hidden`);  
+      setTimeout(() => {
+        this.screenElements[this.activeScreen].classList.add(`active`);
+      }, this.PAGE_ACTIVE_DELAY);
+    }
   }
 
   changeActiveMenuItem() {
@@ -83,10 +107,18 @@ export default class FullPageScroll {
   }
 
   reCalculateActiveScreenPosition(delta) {
+    this.previousScreen = this.activeScreen;
     if (delta > 0) {
       this.activeScreen = Math.min(this.screenElements.length - 1, ++this.activeScreen);
     } else {
       this.activeScreen = Math.max(0, --this.activeScreen);
     }
+  }
+
+  addPageChangeTransition() {
+    return this.previousScreen !== this.activeScreen &&
+      this.transitions.some(
+        transition => this.previousScreen === transition.from && this.activeScreen === transition.to
+      );
   }
 }
