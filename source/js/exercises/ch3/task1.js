19328b02f3acdf331837bd829d2e29490423a438
Egor Prilipko <nero.36@mail.ru>
2022-01-16 20:10:31 +0300

    module3-task2 added secondaryAward animation

--- source/js/exercises/ch3/task1.js
+++ source/js/exercises/ch3/task1.js
@@ -4,15 +4,27 @@ window.addEventListener('load', animationStarter);
 function animationStarter() {
     switch(document.location.hash) {
         case '#prizes':
-            const svg = document.getElementById('primaryAwardAnimation');
-            const animation = document.getElementById('primaryAwardAnimationStart');
-            if (svg && animation) {
-                animation.endElement();
-                svg.setCurrentTime(0);
-                animation.beginElement();
-            }
+            initAnimation('primaryAward');
+
+            setTimeout(() => initAnimation('secondaryAward'), 3800);
+
             break;
         default:
             break;
+    }
+}
+
+function initAnimation(objectWrapperSelector) {
+    const objectWrapper = document.getElementById(objectWrapperSelector);
+
+    if (objectWrapper) {
+        const svg = objectWrapper.contentDocument.getElementById('animation');
+        const animation = objectWrapper.contentDocument.getElementById('animationStart');
+
+        if (svg && animation) {
+            animation.endElement();
+            svg.setCurrentTime(0);
+            animation.beginElement();
+        }
     }
 }
