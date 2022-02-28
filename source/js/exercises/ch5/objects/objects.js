f72dc22c2b4af78b1a7081ecbfd7eabe146144b4
Egor Prilipko <nero.36@mail.ru>
2022-02-28 18:36:40 +0300

    module5-task6: added hue effect animation

--- source/js/exercises/ch5/objects/objects.js
+++ source/js/exercises/ch5/objects/objects.js
@@ -33,6 +33,18 @@ function getHueMatrix(rotation) {
     ];
 }
 
+function getKeyframes(keyframes, time) {
+    const pastKeyframes = keyframes.filter(
+        keyframe => keyframe.time <= time
+    );
+    const futureKeyframes = keyframes.slice(pastKeyframes.length, keyframes.length);
+
+    const fromKeyframe = pastKeyframes[pastKeyframes.length - 1];
+    const toKeyframe = futureKeyframes[0];
+
+    return [ fromKeyframe, toKeyframe ];
+}
+
 const objects = [
     {
         id: TITLE_SCREEN_ID,
@@ -47,7 +59,8 @@ const objects = [
         texture: 'img/exercises/chapter5/story-slide2.png',
         fragment: hueFragmentShader,
         uniforms: {
-            hueMatrix: { type: 'mat4', value: getHueMatrix(345) },
+            // hueMatrix: { type: 'mat4', value: getHueMatrix(345) },
+            hueMatrix: { type: 'mat4', value: getHueMatrix(0) },
             bubblePosition: { type: 'mat3', value: [
                 400.0, 350.0, 0.0,
                 790.0, 850.0, 0.0,
@@ -58,7 +71,50 @@ const objects = [
                 40.0,
                 35.0
             ]}
-        }
+        },
+        animations: [
+            {
+                id: 'hueMatrix',
+                keyframes: [
+                    {
+                        time: 0,
+                        value: getHueMatrix(345)
+                    },
+                    {
+                        time: 750,
+                        value: getHueMatrix(10)
+                    },
+                    {
+                        time: 1500,
+                        value: getHueMatrix(350)
+                    },
+                    {
+                        time: 2000,
+                        value: getHueMatrix(0)
+                    }
+                ],
+                getValue: function getValue(time) {
+                    const [ fromKeyframe, toKeyframe ] = getKeyframes(this.keyframes, time);
+
+                    if (!toKeyframe) {
+                        return fromKeyframe.value;
+                    }
+
+                    const result = [];
+                    
+                    for (let i = 0; i !== fromKeyframe.value.length; i++) {
+                        const fromValue = fromKeyframe.value[i];
+                        const toValue = toKeyframe.value[i];
+
+                        const amount = (time - fromKeyframe.time) / (toKeyframe.time - fromKeyframe.time);
+
+                        result.push((1 - amount) * fromValue + amount * toValue);
+                    }
+
+                    return result;
+                }
+            }
+        ]
     },
     {
         id: STORY_SCREEN_SLIDE3_ID,
